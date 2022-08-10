302df32cb48ab18ff0217496be869af64913ccb6
Md Mazharul Islam Farhad <mazharul383all@gmail.com>
2022-08-10 14:16:10 +0600

    add firebase

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -7,13 +7,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:process.env.React_App_apiKey,
-  authDomain:process.env.React_App_authDomain,
-  projectId:process.env.React_App_projectId,
-  storageBucket:process.env.React_App_storageBucket,
-  messagingSenderId:process.env.React_App_messagingSenderId,
-  appId:process.env.React_App_appId,
+  apiKey: process.env.React_App_apiKey,
+  authDomain: process.env.React_App_authDomain,
+  projectId: process.env.React_App_projectId,
+  storageBucket: process.env.React_App_storageBucket,
+  messagingSenderId: process.env.React_App_messagingSenderId,
+  appId: process.env.React_App_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export default app;
